7b94527f3073705434142908ebf971821fc1f30f
216k155 <32459326+216k155@users.noreply.github.com>
2018-01-27 18:34:59 +1100

    Updated seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -85,11 +85,10 @@ public:
         assert(hashGenesisBlock == uint256("0x00000759bb3da130d7c9aedae170da8335f5a0d01a9007e4c8d3ccd08ace6a42")); 
         assert(genesis.hashMerkleRoot == uint256("0xe08ae0cfc35a1d70e6764f347fdc54355206adeb382446dd54c32cd0201000d3"));
 
-        vSeeds.push_back(CDNSSeedData("sd1", "45.32.245.217"));
-        vSeeds.push_back(CDNSSeedData("sd2", "45.63.25.110"));
-        vSeeds.push_back(CDNSSeedData("sd3", "45.76.118.90"));
-        vSeeds.push_back(CDNSSeedData("sd4", "136.243.89.142"));
-        vSeeds.push_back(CDNSSeedData("sd5", "pool.luxcore.io"));
+        vSeeds.push_back(CDNSSeedData("sd", "45.32.245.217")); // SIN - AU
+        vSeeds.push_back(CDNSSeedData("sd1", "45.76.118.90")); // US - AU
+        vSeeds.push_back(CDNSSeedData("sd2", "45.77.44.147"));
+        vSeeds.push_back(CDNSSeedData("sd3", "207.246.96.37"));
         
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48); // LUX Start letter L
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,48); 
