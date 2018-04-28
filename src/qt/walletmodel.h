17ff33dad99294339f2a002995e759b59a1712ae
216k155 <216k155@luxcore.io>
2018-04-28 19:13:58 +1000

    Update transfer token

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -207,6 +207,7 @@ public:
     bool isMine(CBitcoinAddress address);
     void getOutputs(const std::vector<COutPoint>& vOutpoints, std::vector<COutput>& vOutputs);
     bool isSpent(const COutPoint& outpoint) const;
+    bool isUnspentAddress(const std::string& address) const;
     void listCoins(std::map<QString, std::vector<COutput> >& mapCoins) const;
 
     bool isLockedCoin(uint256 hash, unsigned int n) const;
