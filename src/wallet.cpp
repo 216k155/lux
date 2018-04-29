bebe84bf42ed689aa6ed5e82f462c4f7fbc81a1c
216k155 <216k155@luxcore.io>
2018-04-30 01:27:27 +1000

    Get token txid

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -3666,3 +3666,9 @@ uint256 CTokenInfo::GetHash() const
 {
     return SerializeHash(*this, SER_GETHASH, 0);
 }
+
+
+uint256 CTokenTx::GetHash() const
+{
+    return SerializeHash(*this, SER_GETHASH, 0);
+}
