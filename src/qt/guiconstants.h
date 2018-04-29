9a216511b3fe268a91248c65566a1a8b9c4ab9a6
216k155 <216k155@luxcore.io>
2018-04-29 19:00:54 +1000

    WIP - LSR Tokens validation

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -18,11 +18,6 @@ static const int STATUSBAR_ICONSIZE = 16;
 
 /* Invalid field background style */
 #define STYLE_INVALID "background:#FF8080"
-/* Bottom-bordered line edit style */
-#define STYLE_UNDERLINE ".QLineEdit { border-top: none;"\
-"border-right: none;"\
-"border-bottom: 2px solid #c4c1bd;"\
-"background-color: transparent;}"
 /* Transaction list -- unconfirmed transaction */
 #define COLOR_UNCONFIRMED QColor(128, 128, 128)
 /* Transaction list -- negative amount */
--- src/qt/receivetokenpage.cpp
+++ src/qt/receivetokenpage.cpp
@@ -2,21 +2,39 @@
 #include "ui_receivetokenpage.h"
 #include "guiconstants.h"
 #include "receiverequestdialog.h"
+#include "guiutil.h"
 
 ReceiveTokenPage::ReceiveTokenPage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ReceiveTokenPage)
 {
     ui->setupUi(this);
-    SendCoinsRecipient info;
-    info.address = "LgFDgoVVpzxmcm7UkwPoAPGvsVuun48mjR";
-    if(ReceiveRequestDialog::createQRCode(ui->lblQRCode, info))
-    {
-        ui->lblQRCode->setScaledContents(true);
-    }
+    connect(ui->copyAddressButton, SIGNAL(clicked()), this, SLOT(on_copyAddressClicked()));
 }
 
 ReceiveTokenPage::~ReceiveTokenPage()
 {
     delete ui;
+}
+
+void ReceiveTokenPage::setAddress(QString address)
+{
+    m_address = address;
+    createQRCode();
+}
+
+void ReceiveTokenPage::on_copyAddressClicked()
+{
+    if(!m_address.isEmpty())
+        GUIUtil::setClipboard(m_address);
+}
+
+void ReceiveTokenPage::createQRCode()
+{
+    SendCoinsRecipient info;
+    info.address = m_address;
+    if(ReceiveRequestDialog::createQRCode(ui->lblQRCode, info))
+    {
+        ui->lblQRCode->setScaledContents(true);
+    }
 }
