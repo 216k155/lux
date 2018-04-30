5856988607668f7bb514720a9c420e396986bc6b
216k155 <216k155@luxcore.io>
2018-04-30 20:25:04 +1000

    WIP - Update Lux Standard Request (LSR) token tx record

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -42,6 +42,7 @@
 #if BOOST_FILESYSTEM_VERSION >= 3
 #include <boost/filesystem/detail/utf8_codecvt_facet.hpp>
 #endif
+#include <boost/scoped_array.hpp>
 
 #include <QAbstractItemView>
 #include <QApplication>
@@ -57,6 +58,7 @@
 #include <QTextDocument> // for Qt::mightBeRichText
 #include <QThread>
 #include <QToolButton>
+#include <QMouseEvent>
 
 #if QT_VERSION < 0x050000
 #include <QUrl>
@@ -254,6 +256,19 @@ void copyEntryData(QAbstractItemView* view, int column, int role)
         setClipboard(selection.at(0).data(role).toString());
     }
 }
+
+    void copyEntryDataFromList(QAbstractItemView *view, int role)
+    {
+        if(!view || !view->selectionModel())
+            return;
+        QModelIndexList selection = view->selectionModel()->selectedIndexes();
+
+        if(!selection.isEmpty())
+        {
+            // Copy first item
+            setClipboard(selection.at(0).data(role).toString());
+        }
+    }
 
     QString getEntryData(QAbstractItemView *view, int column, int role)
     {
