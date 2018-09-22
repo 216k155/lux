5d061ef84a0a4c97089f0091b90bfdda11134d78
216k155 <216k155@luxcore.io>
2018-09-23 08:51:04 +1000

    RPC - Add -mempoolexpiry, the expiration time for mempool transactions
    Signed-off-by: 216k155 <216k155@luxcore.io>

--- src/main.h
+++ src/main.h
@@ -159,6 +159,8 @@ static const bool DEFAULT_SPENTINDEX = false;
 static const int64_t STATIC_POS_REWARD = 1 * COIN; //Constant reward 8%
 
 static const bool DEFAULT_LOGEVENTS = false;
+/** Default for -mempoolexpiry */
+static const unsigned int DEFAULT_MEMPOOL_EXPIRY = 72;
 
 ////////////////////////////////////////////////////// lux
 static const uint64_t DEFAULT_GAS_LIMIT_OP_CREATE=2500000;
