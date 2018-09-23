216k155/lux

commit 56d966f476f0f7e3ef37d12ab8011794ae85839b

"Check for update" reminder
Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

src/Makefile.qt.include
@@ -159,6 +159,7 @@ QT_MOC_CPP = \
   qt/moc_transactiontablemodel.cpp \
   qt/moc_transactionview.cpp \
   qt/moc_utilitydialog.cpp \
+  qt/moc_updatedialog.cpp \
   qt/moc_walletframe.cpp \
   qt/moc_walletmodel.cpp \
   qt/moc_walletview.cpp \
@@ -289,6 +290,7 @@ BITCOIN_QT_H = \
   qt/transactiontablemodel.h \
   qt/transactionview.h \
   qt/utilitydialog.h \
+  qt/updatedialog.h \
   qt/walletframe.h \
   qt/walletmodel.h \
   qt/walletmodeltransaction.h \
@@ -411,6 +413,7 @@ BITCOIN_QT_CPP = \
   qt/tokenamountfield.cpp \
   qt/trafficgraphwidget.cpp \
   qt/utilitydialog.cpp \
+  qt/updatedialog.cpp \
   qt/winshutdownmonitor.cpp \
   qt/hexaddressconverter.cpp
 

configure.ac
@@ -6,7 +6,9 @@ define(_CLIENT_VERSION_REVISION, 0)
 define(_CLIENT_VERSION_BUILD, 0)
 define(_CLIENT_VERSION_IS_RELEASE, true)
 define(_COPYRIGHT_YEAR, 2018)
-AC_INIT([Luxcore],[_CLIENT_VERSION_MAJOR._CLIENT_VERSION_MINOR._CLIENT_VERSION_REVISION],[www.luxcore.io],[lux])
+define(_COPYRIGHT_HOLDERS,[The %s developers])
+define(_COPYRIGHT_HOLDERS_SUBSTITUTION,[[Luxcore]])
+AC_INIT([Luxcore],[_CLIENT_VERSION_MAJOR._CLIENT_VERSION_MINOR._CLIENT_VERSION_REVISION],[https://github.com/lux-core/lux/issues],[lux],[https://luxcore.io/])
 AC_CONFIG_SRCDIR([src/main.cpp])
 AC_CONFIG_HEADERS([src/config/lux-config.h])
 AC_CONFIG_AUX_DIR([build-aux])
@@ -1201,13 +1203,24 @@ AC_DEFINE(CLIENT_VERSION_MINOR, _CLIENT_VERSION_MINOR, [Minor version])
 AC_DEFINE(CLIENT_VERSION_REVISION, _CLIENT_VERSION_REVISION, [Build revision])
 AC_DEFINE(CLIENT_VERSION_BUILD, _CLIENT_VERSION_BUILD, [Version Build])
 AC_DEFINE(CLIENT_VERSION_IS_RELEASE, _CLIENT_VERSION_IS_RELEASE, [Version is release])
-AC_DEFINE(COPYRIGHT_YEAR, _COPYRIGHT_YEAR, [Version is release])
+AC_DEFINE(COPYRIGHT_YEAR, _COPYRIGHT_YEAR, [Copyright year])
+AC_DEFINE(COPYRIGHT_HOLDERS, "_COPYRIGHT_HOLDERS", [Copyright holder(s) before %s replacement])
+AC_DEFINE(COPYRIGHT_HOLDERS_SUBSTITUTION, "_COPYRIGHT_HOLDERS_SUBSTITUTION", [Replacement for %s in copyright holders string])
+define(_COPYRIGHT_HOLDERS_FINAL, [patsubst(_COPYRIGHT_HOLDERS, [%s], [_COPYRIGHT_HOLDERS_SUBSTITUTION])])
+AC_DEFINE(COPYRIGHT_HOLDERS_FINAL, "_COPYRIGHT_HOLDERS_FINAL", [Copyright holder(s)])
 AC_SUBST(CLIENT_VERSION_MAJOR, _CLIENT_VERSION_MAJOR)
 AC_SUBST(CLIENT_VERSION_MINOR, _CLIENT_VERSION_MINOR)
 AC_SUBST(CLIENT_VERSION_REVISION, _CLIENT_VERSION_REVISION)
 AC_SUBST(CLIENT_VERSION_BUILD, _CLIENT_VERSION_BUILD)
 AC_SUBST(CLIENT_VERSION_IS_RELEASE, _CLIENT_VERSION_IS_RELEASE)
 AC_SUBST(COPYRIGHT_YEAR, _COPYRIGHT_YEAR)
+AC_SUBST(COPYRIGHT_HOLDERS, "_COPYRIGHT_HOLDERS")
+AC_SUBST(COPYRIGHT_HOLDERS_SUBSTITUTION, "_COPYRIGHT_HOLDERS_SUBSTITUTION")
+AC_SUBST(COPYRIGHT_HOLDERS_FINAL, "_COPYRIGHT_HOLDERS_FINAL")
+AC_SUBST(BITCOIN_DAEMON_NAME)
+AC_SUBST(BITCOIN_GUI_NAME)
+AC_SUBST(BITCOIN_CLI_NAME)
+AC_SUBST(BITCOIN_TX_NAME)
 
 AC_SUBST(RELDFLAGS)
 AC_SUBST(SSE42_CXXFLAGS)

src/clientversion.cpp
@@ -43,10 +43,9 @@ const std::string CLIENT_NAME("Luxcore");
 #endif
 
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
-#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "cbcb549"
-#define GIT_COMMIT_DATE "Tue, 26 Dec 2017 18:03 -1803"
+#define GIT_COMMIT_ID "$Format:%h$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \

src/clientversion.h
@@ -7,28 +7,12 @@
 
 #if defined(HAVE_CONFIG_H)
 #include "config/lux-config.h"
-#else
-
-/**
- * client versioning and copyright year
- */
-
-//! These need to be macros, as clientversion.cpp's and lux*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 5
-#define CLIENT_VERSION_MINOR 2
-#define CLIENT_VERSION_REVISION 3
-#define CLIENT_VERSION_BUILD 0
-
-//! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
-
-/**
- * Copyright year (2009-this)
- * Todo: update this when changing our copyright comments in the source
- */
-#define COPYRIGHT_YEAR 2018
-
 #endif //HAVE_CONFIG_H
+
+// Check that required client information is defined
+#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
+#error Client version information missing: version is not defined by lux-config.h or in any other way
+#endif
 
 /**
  * Converts the parameter X to a string after macro replacement on X has been performed.
@@ -38,7 +22,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The Luxcore Developers"
+#define COPYRIGHT_STR STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
 
 /**
  * luxd-res.rc includes this file, but it cannot cope with real c++ code.
@@ -52,9 +36,9 @@
 #include <vector>
 
 static const int CLIENT_VERSION =
-    1000000 * CLIENT_VERSION_MAJOR  ///
-    + 10000 * CLIENT_VERSION_MINOR  ///
-    + 100 * CLIENT_VERSION_REVISION ///
+1000000 * CLIENT_VERSION_MAJOR
+    + 10000 * CLIENT_VERSION_MINOR
+    + 100 * CLIENT_VERSION_REVISION
     + 1 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;

src/qt/bitcoingui.cpp
@@ -39,6 +39,7 @@
 #include "masternodemanager.h"
 #include "ui_interface.h"
 #include "util.h"
+#include "updatedialog.h"
 
 #include <iostream>
 
@@ -134,6 +135,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle* n
                                                                             explorerWindow(0),
                                                                             hexAddressWindow(0),
                                                                             modalOverlay(0),
+                                                                            updateDialog(0),
                                                                             prevBlocks(0),
                                                                             spinnerFrame(0),
                                                                             platformStyle(platformStyle)
@@ -304,6 +306,17 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle* n
     setStakingStatus();
 
     modalOverlay = new ModalOverlay(this->centralWidget());
+
+    updateDialog = new UpdateDialog(this);
+
+    if(fCheckUpdates && updateDialog->newUpdateAvailable())
+    {
+        QString url = "https://github.com/LUX-Core/lux/releases";
+        QString link = QString("<a href=\\\"\"+ url +\"\\\">\"+ url +\"</a>").arg(NEW_RELEASES, NEW_RELEASES);
+        QString message(tr("New lux-qt version available: <br /> %1. <br />").arg(link));
+        QMessageBox::information(this, tr("Check for updates"), message);
+    }
+
 #ifdef ENABLE_WALLET
     if(enableWallet) {
         connect(walletFrame, SIGNAL(requestedSyncWarningInfo()), this, SLOT(showModalOverlay()));

src/qt/bitcoingui.h
@@ -39,6 +39,7 @@ class ModalOverlay;
 class MasternodeList;
 class HexAddressConverter;
 class CBlockIndex;
+class UpdateDialog;
 
 class CWallet;
 
@@ -153,6 +154,7 @@ private:
     BlockExplorer* explorerWindow;
     HexAddressConverter* hexAddressWindow;
     ModalOverlay* modalOverlay;
+    UpdateDialog* updateDialog;
 
     /** Keep track of previous number of blocks, to detect progress */
     int prevBlocks;

src/qt/forms/optionsdialog.ui
@@ -304,6 +304,13 @@
            </widget>
           </item>
           <item>
+           <widget class="QCheckBox" name="checkUpdates">
+            <property name="text">
+             <string>Check for &amp;updates</string>
+            </property>
+           </widget>
+          </item>
+          <item>
            <widget class="QCheckBox" name="parallelMasterNode">
             <property name="toolTip">
              <string>Coming soon</string>

src/qt/optionsdialog.cpp
@@ -202,6 +202,7 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->notUseChangeAddress, OptionsModel::NotUseChangeAddress);
     mapper->addMapping(ui->walletBackups, OptionsModel::WalletBackups);
     mapper->addMapping(ui->zeroBalanceAddressToken, OptionsModel::ZeroBalanceAddressToken);
+    mapper->addMapping(ui->checkUpdates, OptionsModel::CheckUpdates);
 
     /* Network */
     mapper->addMapping(ui->mapPortUpnp, OptionsModel::MapPortUPnP);

commit 1f2f11752d3dbe807b40bae57e2020fc65646057

travis: fix mingw build
broken in commit 56d966f476f0f7e3ef37d12ab8011794ae85839b

src/Makefile.am
@@ -641,7 +641,7 @@ luxd_CPPFLAGS = $(BITCOIN_INCLUDES)
 luxd_LDFLAGS = $(RELDFLAGS) $(AM_LDFLAGS) $(LIBTOOL_APP_LDFLAGS)
 
 if TARGET_WINDOWS
-luxd_SOURCES += luxd-res.rc
+luxd_SOURCES += luxd-res.rc config/lux-config.h
 endif
 
 luxd_LDADD = \
@@ -666,7 +666,7 @@ lux_cli_SOURCES = lux-cli.cpp
 lux_cli_CPPFLAGS = $(BITCOIN_INCLUDES) $(EVENT_CFLAGS)
 
 if TARGET_WINDOWS
-lux_cli_SOURCES += lux-cli-res.rc
+lux_cli_SOURCES += lux-cli-res.rc config/lux-config.h
 endif
 
 lux_cli_LDADD = \

src/Makefile.qt.include
@@ -525,7 +525,9 @@ RES_CSS = \
 
 RES_MOVIES = $(wildcard $(srcdir)/qt/res/movies/spinner-*.png)
 
+if TARGET_WINDOWS
 BITCOIN_RC = qt/res/lux-qt-res.rc
+endif
 
 BITCOIN_QT_INCLUDES = -I$(builddir)/qt -I$(srcdir)/qt -I$(srcdir)/qt/forms \
   -I$(builddir)/qt/forms

src/init.cpp
@@ -345,7 +345,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += "  -nlogfile=<n>           " + _("Set number of debug log files") + "\n";
     strUsage += "  -loadblock=<file>      " + _("Imports blocks from external blk000??.dat file") + " " + _("on startup") + "\n";
     strUsage += "  -maxorphantx=<n>       " + strprintf(_("Keep at most <n> unconnectable transactions in memory (default: %u)"), DEFAULT_MAX_ORPHAN_TRANSACTIONS) + "\n";
-    strUsage += "  -mempoolexpiry=<n>     " + strprintf(_("Do not keep transactions in the mempool longer than <n> hours (default: %u)"), DEFAULT_MEMPOOL_EXPIRY);
+    strUsage += "  -mempoolexpiry=<n>     " + strprintf(_("Do not keep transactions in the mempool longer than <n> hours (default: %u)"), DEFAULT_MEMPOOL_EXPIRY) + "\n";
     strUsage += "  -par=<n>               " + strprintf(_("Set the number of script verification threads (%u to %d, 0 = auto, <0 = leave that many cores free, default: %d)"), -(int)boost::thread::hardware_concurrency(), MAX_SCRIPTCHECK_THREADS, DEFAULT_SCRIPTCHECK_THREADS) + "\n";
 #ifndef WIN32
     strUsage += "  -pid=<file>            " + strprintf(_("Specify pid file (default: %s)"), "luxd.pid") + "\n";

src/lux-cli-res.rc
@@ -1,4 +1,5 @@
 #include <windows.h>             // needed for VERSIONINFO
+#include "config/lux-config.h"
 #include "clientversion.h"       // holds the needed client version information
 
 #define VER_PRODUCTVERSION     CLIENT_VERSION_MAJOR,CLIENT_VERSION_MINOR,CLIENT_VERSION_REVISION,CLIENT_VERSION_BUILD

src/luxd-res.rc
@@ -1,4 +1,5 @@
 #include <windows.h>             // needed for VERSIONINFO
+#include "config/lux-config.h"
 #include "clientversion.h"       // holds the needed client version information
 
 #define VER_PRODUCTVERSION     CLIENT_VERSION_MAJOR,CLIENT_VERSION_MINOR,CLIENT_VERSION_REVISION,CLIENT_VERSION_BUILD

src/qt/res/lux-qt-res.rc
@@ -2,7 +2,8 @@ IDI_ICON1 ICON DISCARDABLE "icons/bitcoin.ico"
 IDI_ICON2 ICON DISCARDABLE "icons/bitcoin_testnet.ico"
 
 #include <windows.h>             // needed for VERSIONINFO
-#include "../../clientversion.h" // holds the needed client version information
+#include <config/lux-config.h>
+#include <clientversion.h>       // holds the needed client version information
 
 #define VER_PRODUCTVERSION     CLIENT_VERSION_MAJOR,CLIENT_VERSION_MINOR,CLIENT_VERSION_REVISION,CLIENT_VERSION_BUILD
 #define VER_PRODUCTVERSION_STR STRINGIZE(CLIENT_VERSION_MAJOR) "." STRINGIZE(CLIENT_VERSION_MINOR) "." STRINGIZE(CLIENT_VERSION_REVISION) "." STRINGIZE(CLIENT_VERSION_BUILD)